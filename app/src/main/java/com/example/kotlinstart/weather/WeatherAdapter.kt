34b944614fdded5a2273e9354e7aadcb258b6e9e
iliassemeonidis <paleologos@mail.ru>
2021-05-21 20:24:06 +0300

    доработка по дз

--- app/src/main/java/com/example/kotlinstart/weather/WeatherAdapter.kt
+++ app/src/main/java/com/example/kotlinstart/weather/WeatherAdapter.kt
@@ -10,28 +10,26 @@ import com.example.kotlinstart.R
 internal class WeatherAdapter(
     private var weatherList: ArrayList<Weather>,
     private val onClickItem: WeatherFragment.OnClickItem
-) : RecyclerView.Adapter<WeatherAdapter.MainFragmentWeatherList>() {
+) : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainFragmentWeatherList =
-        MainFragmentWeatherList(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder =
+        WeatherViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_city_weather, parent, false)
         )
 
     override fun getItemCount() = weatherList.size
 
-    override fun onBindViewHolder(holder: MainFragmentWeatherList, position: Int) {
+    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
         holder.bind(weatherList[position])
 
     }
 
-  internal class MainFragmentWeatherList(itemView: View) : RecyclerView.ViewHolder(itemView) {
+   inner class WeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         private var city: TextView = itemView.findViewById(R.id.text_view_city)
         private var region: TextView = itemView.findViewById(R.id.text_view_region)
         private var temp: TextView = itemView.findViewById(R.id.temperature)
-
         init {
-            itemView.setOnClickListener{}
+            itemView.setOnClickListener{ onClickItem.onClick(weatherList[adapterPosition])}
         }
 
         fun bind(weather: Weather) {
@@ -39,5 +37,7 @@ internal class WeatherAdapter(
             temp.text = weather.temperature
             region.text = weather.region
         }
+
+
     }
 }
